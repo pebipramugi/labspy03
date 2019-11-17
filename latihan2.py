aee5eb4b8f75d1b1d9f3fd27b6e04ec48b9da7c6
pebipramugi <56243487+pebipramugi@users.noreply.github.com>
2019-11-17 13:39:12 +0700

    file project phycram

---
+++ latihan2.py
@@ -0,0 +1,9 @@
+a = int()
+maks  = 0
+while a >= 0:
+    a = int(input("masukan bulangan :"))
+    if a > maks :
+            mak = a
+    if a == 0:
+      break
+print("angka terbesar adalah: ",maks)
